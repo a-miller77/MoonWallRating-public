3dede503c3f9a20c4a873b1a807871ae932f3570
Aiden" "Miller <aiden5077@gmail.com>
2023-04-10 12:03:56 -0500

    created a scalable lr scheduler

--- model_definition.py
+++ model_definition.py
@@ -180,3 +180,30 @@ class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
          return cls(**config)
 
 
+class transformerSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
+    def __init__(self, d_model, warmup_steps=4000, ramp_scalar=1, decay_scalar=1):
+        super().__init__()
+
+        self.d_model = d_model
+        self.d_model = tf.cast(self.d_model, tf.float32)
+        self.ramp_scalar = ramp_scalar
+        self.decay_scalar = decay_scalar
+        
+        self.warmup_steps = warmup_steps
+
+    def __call__(self, step):
+        step = tf.cast(step, dtype=tf.float32)
+        arg1 = tf.math.rsqrt(step) * self.decay_scalar
+        arg2 = step * (self.warmup_steps ** -1.5) * self.ramp_scalar
+        return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
+    
+    def get_config(self):
+        return {
+            "d_model": self.d_model,
+            "warmup_steps": self.warmup_steps,
+        }
+    
+    def from_config(cls, config):
+         return cls(**config)
+
+
